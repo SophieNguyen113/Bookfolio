9c4fa68f2d5e5fbf39575e6a6200b9ea6ff275d8
quocIntern <qle17@student.gsu.edu>
2025-03-22 09:36:25 -0400

    feat: Set up Tailwind CSS and basic components

---
+++ Frontend/bookfolio-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-8 bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Welcome to Bookfolio!</h1>
+    </div>
+  );
+}
+
+export default App
