e5a22f2e2e66260118b4f3adfc8b2618c325b3fe
quocIntern <q.le102100@gmail.com>
2025-03-28 14:29:11 -0400

    Logo Changed
    Changed the Logo to a Book.svg image

--- client/src/sections/Header.jsx
+++ client/src/sections/Header.jsx
@@ -5,6 +5,7 @@ export default function Header({user, api_url}) {
   return (
     <header className="header flex justify-between items-center p-2 px-12 fixed w-full top-0 left-0 z-10" 
     style={{color: 'white', backgroundColor: 'black'}}>
+      
       <h1 className="text-center" style={{color: 'white'}}>Bookfolio</h1>
       <Nav user={user} api_url={api_url}/>
       {user && user.id ? <UserAvatar user={user} /> : null}
